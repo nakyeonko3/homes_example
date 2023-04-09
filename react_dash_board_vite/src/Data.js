d1f8363599aa8208a906ff1ba0b4c373980746d4
nakyeonko3 <badaclock@gmail.com>
2023-04-09 16:18:54 +0900

    project: add vite dash board react

---
+++ react_dash_board_vite/src/Data.js
@@ -0,0 +1,38 @@
+export const UserData = [
+  {
+    id: 1,
+    year: 2016,
+    userGain: 80000,
+    userLost: 823,
+  },
+  {
+    id: 1,
+    year: 2016,
+    userGain: 80000,
+    userLost: 823,
+  },
+  {
+    id: 2,
+    year: 2017,
+    userGain: 40790,
+    userLost: 8213,
+  },
+  {
+    id: 3,
+    year: 2006,
+    userGain: 70000,
+    userLost: 3323,
+  },
+  {
+    id: 4,
+    year: 2016,
+    userGain: 12000,
+    userLost: 823,
+  },
+  {
+    id: 5,
+    year: 2006,
+    userGain: 8200,
+    userLost: 8230,
+  },
+];
