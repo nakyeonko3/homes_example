f061626f6e93acc2e357b616db90e73dad96c950
nakyeonko3 <badaclock@gmail.com>
2023-04-18 16:53:07 +0900

    project: 프로젝트 안의  폴더들  위치 변경, etc 폴더에 현재 프로젝트와 무관한 내용들 넣음

--- flask-mqtt-dashboard-capstone/README.md
+++
@@ -1,4 +0,0 @@
-# flask_mqtt_socket_
-
-## 기능
-socket이라 써놨는데 소켓이 아님
--- model_test/mode_test__.py
+++
@@ -1,6 +0,0 @@
-import joblib
-
-load_model = joblib.load("df_clf.pkl")
-pred = load_model.predict([[6.8, 3.0, 5.5, 2.1]])
-result = pred
-print(result)
